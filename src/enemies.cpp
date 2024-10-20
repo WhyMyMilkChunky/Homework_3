0af019a97fa96941ec32baa6b80ce12ffc9bb0e5
WhyMyMilkChunky <baconpsychedelic13@gmail.com>
2024-10-20 00:33:56 -0400

    added healthbars
    I STG
    
    im not retarded

--- src/enemies.cpp
+++ src/enemies.cpp
@@ -9,52 +9,53 @@ constexpr int DAMAGE_PER_ENEMY = 10;
 
 extern int currentHealth;
 
-
 Enemy CreateEnemy(EnemyType type, Vector2 startPosition) {
+    // Oh look, another function to create enemies. Exciting, right?
     Enemy enemy;
     enemy.position = startPosition;
     enemy.currentWaypoint = 0;
     enemy.type = type;
 
+    // let's give each enemy type some arbitrary stats. No favoritism here, I swear.
     switch (type) {
     case EnemyType::BASIC:
-        enemy.health = 100;
+        enemy.health = enemy.maxHealth = 100;
         enemy.speed = 100.0f;
         break;
     case EnemyType::WALLACE:
-        enemy.health = 50;
+        enemy.health = enemy.maxHealth = 50;
         enemy.speed = 150.0f;
         break;
     case EnemyType::JOSS:
-        enemy.health = 300;
+        enemy.health = enemy.maxHealth = 300;
         enemy.speed = 50.0f;
         break;
     case EnemyType::RUI:
-        enemy.health = 20;
+        enemy.health = enemy.maxHealth = 20;
         enemy.speed = 200.0f;
         break;
     case EnemyType::CONNOR:
-        enemy.health = 500;
+        enemy.health = enemy.maxHealth = 500;
         enemy.speed = 75.0f;
         break;
     }
 
+    //congrats, enemy is born
     return enemy;
 }
-
 
 void UpdateEnemies(std::vector<Enemy>& enemies, const std::vector<Cell>& waypoints, float dt) {
     enemies.erase(
         std::remove_if(enemies.begin(), enemies.end(), [&](Enemy& enemy) {
             int curr = enemy.currentWaypoint;
+
+            // did the enemy finally make it?
             if (curr >= waypoints.size() - 1) {
                 currentHealth -= DAMAGE_PER_ENEMY;
-                if (currentHealth <= 0) {
-                    currentHealth = 0;
-                }
-                return true;
+                return true;  // and poof, the enemy is gone
             }
 
+            //they really don't know when to quit.
             int next = curr + 1;
             Vector2 A = TileCenter(waypoints[curr]);
             Vector2 B = TileCenter(waypoints[next]);
@@ -66,18 +67,18 @@ void UpdateEnemies(std::vector<Enemy>& enemies, const std::vector<Cell>& waypoin
                 enemy.currentWaypoint = next;
             }
 
+            //one way ticket to oblivion.
             return enemy.health <= 0;
             }),
         enemies.end()
     );
 }
 
-
 void DrawEnemies(const std::vector<Enemy>& enemies, Texture2D enemyTexture) {
     for (const Enemy& enemy : enemies) {
         Color tint;
 
-        //tint based on type
+        //no favorites here, nuh uh, nope, not at all
         switch (enemy.type) {
         case EnemyType::BASIC:
             tint = WHITE;
@@ -91,18 +92,61 @@ void DrawEnemies(const std::vector<Enemy>& enemies, Texture2D enemyTexture) {
         case EnemyType::RUI:
             tint = YELLOW;
             break;
-        case EnemyType::CONNOR:
+        case EnemyType::CONNOR://this must be a really cool EnemyType
             tint = RED;
             break;
         }
-
-        
         DrawTexturePro(enemyTexture,
             { 0, 0, (float)enemyTexture.width, (float)enemyTexture.height },
+
             { enemy.position.x, enemy.position.y, ENEMY_RADIUS * 2, ENEMY_RADIUS * 2 },
             { ENEMY_RADIUS, ENEMY_RADIUS },
             0,
             tint
         );
+
+        DrawEnemyHealthBar(enemy.position, enemy.health, enemy.maxHealth, ENEMY_RADIUS);
     }
+}
+
+void DrawEnemyHealthBar(Vector2 enemyPosition, int currentHealth, int maxHealth, float enemyRadius) {
+    //makehealth bars proportional
+    const int BASE_HEALTH_BAR_WIDTH = 50;
+    const int HEALTH_BAR_HEIGHT = 5;
+    const int HEALTH_BAR_OUTLINE_THICKNESS = 2;
+    const int MIN_HEALTH_BAR_WIDTH = 30;
+    const int MAX_HEALTH_BAR_WIDTH = 150;
+
+    //bar size
+    float healthPercentage = (float)currentHealth / (float)maxHealth;
+    int healthBarWidth = BASE_HEALTH_BAR_WIDTH + (maxHealth / 50);
+
+    if (healthBarWidth < MIN_HEALTH_BAR_WIDTH)
+        healthBarWidth = MIN_HEALTH_BAR_WIDTH;
+    else if (healthBarWidth > MAX_HEALTH_BAR_WIDTH)
+        healthBarWidth = MAX_HEALTH_BAR_WIDTH;
+
+    //inner health bar
+    int innerBarWidth = (int)(healthBarWidth * healthPercentage);
+    int posX = (int)(enemyPosition.x - healthBarWidth / 2);
+    int posY = (int)(enemyPosition.y - enemyRadius - 10);
+
+    // is the enemy almost dead? we reflect that visually, because we’re nice like that
+    Color healthColor;
+    if (healthPercentage > 0.7f)
+        healthColor = GREEN;
+    else if (healthPercentage > 0.3f)
+        healthColor = YELLOW;
+    else
+        healthColor = RED;
+
+    //outlines look better on UI
+    Rectangle outerRect = { (float)posX - HEALTH_BAR_OUTLINE_THICKNESS, (float)posY - HEALTH_BAR_OUTLINE_THICKNESS,
+                            (float)healthBarWidth + 2 * HEALTH_BAR_OUTLINE_THICKNESS,
+                            (float)HEALTH_BAR_HEIGHT + 2 * HEALTH_BAR_OUTLINE_THICKNESS };
+    DrawRectangleRec(outerRect, BLACK);
+
+    //the part that shows you how much pain they’re in
+    Rectangle innerRect = { (float)posX, (float)posY, (float)innerBarWidth, (float)HEALTH_BAR_HEIGHT };
+    DrawRectangleRec(innerRect, healthColor);
 }
--- src/enemies.h
+++ src/enemies.h
@@ -17,10 +17,12 @@ struct Enemy {
     Vector2 direction;
     float speed;
     int health;
+    int maxHealth;
     int currentWaypoint;
     EnemyType type;
 };
 
 void UpdateEnemies(std::vector<Enemy>& enemies, const std::vector<Cell>& waypoints, float dt);
 void DrawEnemies(const std::vector<Enemy>& enemies, Texture2D enemyTexture);
+void DrawEnemyHealthBar(Vector2 enemyPosition, int currentHealth, int maxHealth, float enemyRadius);
 Enemy CreateEnemy(EnemyType type, Vector2 startPosition);
